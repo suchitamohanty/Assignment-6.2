84278a6f8a89dcd397ec2c39bfabca0e65510d6b
Suchita Mohanty <mohanty.suchita@gmail.com>
2017-01-23 09:10:50 +0530

    a JavaScript program to find the greatest among 3 numbers

---
+++ Readme.md
@@ -0,0 +1,3 @@
+Session 5: JavaScript
+Assignment – Write a JavaScript program to find the greatest
+among 3 numbers and display the greatest number in console.
---
+++ js/app.js
@@ -0,0 +1,9 @@
+var a = 10, b = 50, c = 30;
+
+if(a > b  && a > c) {
+  console.log("A is greatest");
+} else if( b > c ) {
+  console.log("B is greatest");
+} else {
+  console.log("C is greatest");
+}
